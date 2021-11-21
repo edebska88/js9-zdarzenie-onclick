bb24acaf3a13a531e7ce36ae96d1cae290bbcfa1
Emilia Debska <emiliad88@gmail.com>
2021-11-22 00:37:53 +0100

    zdarzenie onclick

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('button');
+button.onclick = function () {
+    alert('Klik działa!')
+};
+console.log(button);
+
+//krócej: document.querySelector('button').onclick = function() {alert('Klik działa!')};
